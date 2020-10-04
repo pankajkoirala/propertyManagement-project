e9f12b111ed95c1f1d37e0a15b95a56d59aac886
pankajkoirala <pankaj.koirala.77@gmail.com>
2020-10-04 12:32:07 +0545

    login made

--- src/container/login/login .js
+++ src/container/login/login .js
@@ -1,9 +1,32 @@
-import React from "react"
-import loginComponent from "../../component/login/login" 
+import React,{useState} from "react"
+import Axios from "axios"
+import LoginComponent from "../../component/login/login" 
+import {setLocalStorage} from "../../const/tokenStorage"
+import {base_URL} from "../../const/base_URL"
+
+
 
 const LoginContainer=()=>{
+    const[response,setResponse]=useState("")
+
+    const login=(data)=>{
+        Axios({
+          method: 'post',
+          url: base_URL+'/api/login',
+          data: data,
+          config: { headers: {'Content-Type': 'application/x-www-form-urlencoded',"Access-Control-Allow-Origin": "*", }}
+          }).then((res)=>{
+            setResponse(res.data.message);
+            setLocalStorage("token",res.data.token)
+            console.log(res.data);
+        }).catch((err)=>{
+            console.log(err);
+          setResponse("username and password doesnt match");
+        })}
     return(
-        <div><loginComponent/></div>
+        <div><LoginComponent
+        login={login}
+        /></div>
     )
 }
 export default LoginContainer
