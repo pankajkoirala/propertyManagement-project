8db6bbb0b3a93a9d8650ab5eabc50d99ace0da02
sulochanapoudel <sulochanamuna@gmail.com>
2020-10-19 13:50:33 +0545

    cheque managed

---
+++ src/container/entryForm/cheque/chequeEntry/chequeEntryForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ChequeEntryComponent from "../../../../component/entryForm/cheque/chequeEntry/chequeEntryForm.js"
+
+
+
+
+const ChequeEntryContainer=()=> {
+  return(<div>
+    <ChequeEntryComponent/>
+  </div>)
+}
+
+export default ChequeEntryContainer
