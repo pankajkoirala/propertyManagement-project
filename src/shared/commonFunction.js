7fb4b38b9c067926945aa291d1108f005c26ca40
pankajkoirala <pankaj.koirala.77@gmail.com>
2020-11-20 11:59:45 +0545

    bardiagram finished

--- src/shared/commonFunction.js
+++ src/shared/commonFunction.js
@@ -1,5 +1,23 @@
+import moment from "moment";
+
 export const reloadFunction = () => {
   setTimeout(() => {
     window.location.reload();
   }, 1500);
+};
+
+export const incomeCalc = (allCheque, setData, month, addnum) => {
+  let monthIncome = 0;
+  let filterCheque = allCheque.filter(
+    (arg) =>
+      moment(arg.cheque_issueDate).format("YYYY-MM") === `${addnum}-${month}`
+  );
+  for (let index = 0; index < filterCheque.length; index++) {
+    monthIncome =
+      monthIncome +
+      filterCheque[index].miscellaneous_amount +
+      filterCheque[index].vat_amount +
+      filterCheque[index].cheque_amount;
+  }
+  setData.push(monthIncome);
 };
