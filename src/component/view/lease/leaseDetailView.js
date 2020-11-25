b7f25cb3a64ccc85f13667b925f323d027937dc5
pankajkoirala <pankaj.koirala.77@gmail.com>
2020-11-25 12:34:51 +0545

    detail data show making

--- src/component/view/lease/leaseDetailView.js
+++ src/component/view/lease/leaseDetailView.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import LeaseEntryForm from "../../entryForm/lease/lease";
-import moment from "moment";
 import PoopUp from "./../../../shared/popup";
 import TopNavBar from "../../../shared/topNavBar";
+import { Table } from "reactstrap";
+import moment from "moment";
 
 let LeaseDetailViewComponent = (props) => {
   const [showEditForm, setShowEditForm] = useState(false);
@@ -12,52 +13,222 @@ let LeaseDetailViewComponent = (props) => {
     setShowEditForm(!showEditForm);
   };
 
-  if (props?.selecteOneLease?.length > 0) {
-    let commerceDate = moment(props?.selecteOneLease[0]?.commenceDate);
-    let expireDate = moment(props?.selecteOneLease[0]?.expirationDate);
-    let difference = expireDate.diff(commerceDate, "days"); // resul
-  }
-
   return (
     <>
-      <TopNavBar/>
+      <TopNavBar />
       <div>
         {showEditForm === false ? (
           props.selecteOneLease.map((arg, index) => {
             return (
               <div key={index} className="property-card">
-                <div className="card-contents">
-                  <div className="">
+                <div className="row">
+                  <div className="col-6">
                     <img
-                      className="Propertyimage"
-                      src={arg.photo}
+                      style={{
+                        height: "400px",
+                        width: "100%",
+                        borderRadius: "20px 20px 0px 0px",
+                      }}
+                      src={arg?.property?.files_list[0]?.file}
                       alt="recently added"
                     />
                   </div>
-                  <div className="property-desc">
-                    <h5>{arg.property_type}</h5>
-                    <p>For: {arg.property_status}</p>
-                    <p>
-                      <i className="fa fa-map-marker"></i> {arg.country},
-                      {arg.city}
-                    </p>
-                    <p>
-                      Rs. <b>{arg.property_price}</b> per Month
-                    </p>
+                  <div className="col-6">
+                    <h5 className="text-center mb-2">Property Information</h5>
+                    <div className="d-flex justify-content-between mr-4">
+                      <div style={{ left: "0px" }}>
+                        <div className="font-weight-bold my-2">Propety Id</div>
+                        <div className="font-weight-bold my-2">
+                          Propety Type
+                        </div>
+
+                        <div className="font-weight-bold my-2">Area</div>
+                        <div className="font-weight-bold my-2">
+                          Building Name
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          Building Number
+                        </div>
+                        <div className="font-weight-bold my-2">Plot Number</div>
+                        <div className="font-weight-bold my-2">
+                          {" "}
+                          Building Floor Number
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          Municipality Number
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          Property Premise Number
+                        </div>
+                        <div className="font-weight-bold my-2">community </div>
+                        <div className="font-weight-bold my-2">
+                          Property Owner Name
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          Property Area
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-weight-bold my-2">
+                          {arg?.property?.referenceNO}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {arg?.property?.property_type}
+                        </div>
+
+                        <div className="font-weight-bold my-2">
+                          {arg?.property?.area}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {arg?.property?.building_Name}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {arg?.property?.building_Number}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {arg?.property?.plot_Number}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {arg?.property?.building_floorNumber}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {arg?.property?.Muncipality_Number}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {arg?.property?.Property_Premise_Number}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {arg?.property?.property_community}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {arg?.property?.Property_ownerName}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {arg?.property?.Property_Area}
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <button onClick={() => setShowEditForm(!showEditForm)}>
-                    edit
-                  </button>
-                  <button onClick={() => setShowPopUp(true)}>delete</button>
-                  <PoopUp
-                    isOpen={showPopup}
-                    isClose={setShowPopUp}
-                    CRUD_Function={props.LeaseDelete}
-                    id={arg._id}
-                    buttonName={"Delete"}
-                    message={"are you sure want to delete"}
-                  />
                 </div>
+                <div className="row">
+                  <div className="col-6">
+                    <h5 className="text-center mb-2">Lease Information</h5>
+                    <div className="d-flex justify-content-between mr-4">
+                      <div style={{ left: "0px" }}>
+                        <div className="font-weight-bold my-2">Lease ID</div>
+                        <div className="font-weight-bold my-2">
+                          Commerce Date
+                        </div>
+                        <div className="font-weight-bold my-2">Expiration</div>
+                        <div className="font-weight-bold my-2">
+                          Security Deposite
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          Security Due Date
+                        </div>
+                        <div className="font-weight-bold my-2">Lease Term</div>
+                        <div className="font-weight-bold my-2">Frequency</div>
+                      </div>
+                      <div>
+                        <div className="font-weight-bold my-2">
+                          {arg.LeaseId}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {moment(arg.commenceDate).format("YYYY-MM-DD")}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {moment(arg.expirationDate).format("YYYY-MM-DD")}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {arg.securityDeposite}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {moment(arg.securityfirstDueDate).format(
+                            "YYYY-MM-DD"
+                          )}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {arg.lease_Term}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {arg.frequency === "7"
+                            ? "Weekly"
+                            : arg.frequency === "14"
+                            ? "Bi-Weekly"
+                            : arg.frequency === "30"
+                            ? "Monthly"
+                            : arg.frequency === "90"
+                            ? "Quartely"
+                            : "Yearly"}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-6">
+                    <h5 className="text-center mb-2">Tenant Information</h5>
+                    <div className="d-flex justify-content-between mr-4">
+                      <div style={{ left: "0px" }}>
+                        <div className="font-weight-bold my-2">Tenant ID</div>
+                        <div className="font-weight-bold my-2">Tenant Name</div>
+                        <div className="font-weight-bold my-2">
+                          Date Of Birth/Company Registration Date
+                        </div>
+                        <div className="font-weight-bold my-2">Address</div>
+                        <div className="font-weight-bold my-2">Email </div>
+                        <div className="font-weight-bold my-2">Contact</div>
+                        <div className="font-weight-bold my-2">Gov Id No</div>
+                        <div className="font-weight-bold my-2">
+                          Driving License
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-weight-bold my-2">
+                          {arg?.tenants?.TenantId}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {arg?.tenants?.tenant_Name}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {moment(
+                            arg?.tenants?.DateOfBirth_registrationDate
+                          ).format("YYYY-MM-DD")}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {arg?.tenants?.area +
+                            "," +
+                            arg?.tenants?.city +
+                            "," +
+                            arg?.tenants?.country}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {arg?.tenants?.tenant_email}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {arg?.tenants?.tenant_phoneNo}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {arg?.tenants?.tenant_GovIdNo}
+                        </div>
+                        <div className="font-weight-bold my-2">
+                          {arg?.tenants?.tenant_DrivingLicenceNo}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <button onClick={() => setShowEditForm(!showEditForm)}>
+                  edit
+                </button>
+                <button onClick={() => setShowPopUp(true)}>delete</button>
+                <PoopUp
+                  isOpen={showPopup}
+                  isClose={setShowPopUp}
+                  CRUD_Function={props.LeaseDelete}
+                  id={arg._id}
+                  buttonName={"Delete"}
+                  message={"are you sure want to delete"}
+                />
               </div>
             );
           })
--- src/component/view/maintananceTicket/maintananceTicketDetailView.js
+++ src/component/view/maintananceTicket/maintananceTicketDetailView.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import MaintananceTicketEntryForm from "../../entryForm/maintananceTicket/maintananceTicket";
 import PoopUp from "./../../../shared/popup";
 import TopNavBar from "../../../shared/topNavBar";
+import moment from "moment";
 
 let MaintananceTicketDetailViewComponent = (props) => {
   const [showEditForm, setShowEditForm] = useState(false);
@@ -12,51 +13,208 @@ let MaintananceTicketDetailViewComponent = (props) => {
   };
   return (
     <>
-      <TopNavBar/>
+      <TopNavBar />
       <div>
         {showEditForm === false ? (
           props.selectedMaintananceTicket.map((arg, index) => {
             return (
               <div key={index} className="property-card">
-                <div className="card-contents">
-                  <div className="">
+                <div className="row">
+                  <div className="col-6">
                     <img
-                      className="Propertyimage"
-                      src={arg.photo}
+                      style={{
+                        height: "400px",
+                        width: "100%",
+                        borderRadius: "20px 20px 0px 0px",
+                      }}
+                      src={arg?.MaintananceCompanyId?.files_list[0]?.file}
                       alt="recently added"
                     />
                   </div>
-                  <div className="property-desc">
-                    <h5>{arg.property_type}</h5>
-                    <p>For: {arg.property_status}</p>
-                    <p>
-                      <i className="fa fa-map-marker"></i> {arg.country},
-                      {arg.city}
-                    </p>
-                    <p>
-                      Rs. <b>{arg.property_price}</b> per Month
-                    </p>
+                  <div className="col-6">
+                    <h5 className="text-center mb-4">
+                      Maintanance Company Information
+                    </h5>
+                    <div className="d-flex justify-content-between mr-4">
+                      <div style={{ left: "0px" }}>
+                        <div className="font-weight-bold my-1">
+                          Maintanance Company ID
+                        </div>
+                        <div className="font-weight-bold my-1">
+                          Company Name
+                        </div>
+                        <div className="font-weight-bold my-1">Area</div>
+                        <div className="font-weight-bold my-1">City</div>
+                        <div className="font-weight-bold my-1">Country</div>
+                        <div className="font-weight-bold my-1">
+                          Registration Number
+                        </div>
+                        <div className="font-weight-bold my-1">
+                          Registration Date
+                        </div>
+                        <div className="font-weight-bold my-1">Email </div>
+                        <div className="font-weight-bold my-1">
+                          Phone Number
+                        </div>
+                        <div className="font-weight-bold my-1">
+                          Mobile Number
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-weight-bold my-1">
+                          {arg?.MaintananceCompanyId?.Company_ID}
+                        </div>
+                        <div className="font-weight-bold my-1">
+                          {arg?.MaintananceCompanyId?.Company_Name}
+                        </div>
+                        <div className="font-weight-bold my-1">
+                          {arg?.MaintananceCompanyId?.Company_area}
+                        </div>
+                        <div className="font-weight-bold my-1">
+                          {arg?.MaintananceCompanyId?.Company_city}
+                        </div>
+                        <div className="font-weight-bold my-1">
+                          {arg?.MaintananceCompanyId?.Company_country}
+                        </div>
+
+                        <div className="font-weight-bold my-1">
+                          {
+                            arg?.MaintananceCompanyId
+                              ?.Company_Registration_Number
+                          }
+                        </div>
+                        <div className="font-weight-bold my-1">
+                          {moment(
+                            arg?.MaintananceCompanyId
+                              ?.Company_Registeration_Date
+                          ).format("YYYY-MM-DD")}
+                        </div>
+                        <div className="font-weight-bold my-1">
+                          {arg?.MaintananceCompanyId?.Company_email}
+                        </div>
+
+                        <div className="font-weight-bold my-1">
+                          {arg?.MaintananceCompanyId?.Company_phoneNo}
+                        </div>
+                        <div className="font-weight-bold my-1">
+                          {arg?.MaintananceCompanyId?.Company_Mobile_Number}
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <button onClick={() => setShowEditForm(!showEditForm)}>
-                    edit
-                  </button>
-                  <button
-                    className="danger ml-2"
-                    onClick={() => {
-                      setShowPopUp(true);
-                    }}
-                  >
-                    Delete
-                  </button>
-                  <PoopUp
-                    isOpen={showPopup}
-                    isClose={setShowPopUp}
-                    CRUD_Function={props.maintananceTicketDelete}
-                    id={arg._id}
-                    buttonName={"Delete"}
-                    message={"are you sure want to delete"}
-                  />
                 </div>
+                <h5 className="text-center mb-4 mt-4">
+                  Management Company Information
+                </h5>
+                <div className="row">
+                  <div className="col-6">
+                    <img
+                      style={{
+                        height: "400px",
+                        width: "100%",
+                        borderRadius: "20px",
+                      }}
+                      src={arg?.managementCompanyId?.files_list[0]?.file}
+                      alt="recently added"
+                    />
+                  </div>
+                  <div className="col-6">
+                    <h5 className="text-center mb-4">
+                      Management Company Information
+                    </h5>
+                    <div className="d-flex justify-content-between mr-4">
+                      <div style={{ left: "0px" }}>
+                        <div className="font-weight-bold my-1">
+                          Management Company ID
+                        </div>
+                        <div className="font-weight-bold my-1">
+                          Company Name
+                        </div>
+                        <div className="font-weight-bold my-1">Area</div>
+                        <div className="font-weight-bold my-1">City</div>
+                        <div className="font-weight-bold my-1">Country</div>
+                        <div className="font-weight-bold my-1">
+                          Registration Number
+                        </div>
+                        <div className="font-weight-bold my-1">
+                          Registration Date
+                        </div>
+                        <div className="font-weight-bold my-1">Email </div>
+                        <div className="font-weight-bold my-1">
+                          Phone Number
+                        </div>
+                        <div className="font-weight-bold my-1">
+                          Mobile Number
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-weight-bold my-1">
+                          {
+                            arg?.managementCompanyId
+                              ?.managementCompany_companyID
+                          }
+                        </div>
+                        <div className="font-weight-bold my-1">
+                          {arg?.managementCompanyId?.managementCompany_name}
+                        </div>
+                        <div className="font-weight-bold my-1">
+                          {arg?.managementCompanyId?.managementCompany_area}
+                        </div>
+                        <div className="font-weight-bold my-1">
+                          {arg?.managementCompanyId?.managementCompany_city}
+                        </div>
+                        <div className="font-weight-bold my-1">
+                          {arg?.managementCompanyId?.managementCompany_country}
+                        </div>
+
+                        <div className="font-weight-bold my-1">
+                          {
+                            arg?.managementCompanyId
+                              ?.managementCompany_Registeration_Number
+                          }
+                        </div>
+                        <div className="font-weight-bold my-1">
+                          {moment(
+                            arg?.managementCompanyId
+                              ?.managementCompany_Registeration_Date
+                          ).format("YYYY-MM-DD")}
+                        </div>
+                        <div className="font-weight-bold my-1">
+                          {arg?.managementCompanyId?.managementCompany_email}
+                        </div>
+
+                        <div className="font-weight-bold my-1">
+                          {arg?.managementCompanyId?.managementCompany_phoneNo}
+                        </div>
+                        <div className="font-weight-bold my-1">
+                          {
+                            arg?.managementCompanyId
+                              ?.managementCompany_MobileNumber
+                          }
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <button onClick={() => setShowEditForm(!showEditForm)}>
+                  edit
+                </button>
+                <button
+                  className="danger ml-2"
+                  onClick={() => {
+                    setShowPopUp(true);
+                  }}
+                >
+                  Delete
+                </button>
+                <PoopUp
+                  isOpen={showPopup}
+                  isClose={setShowPopUp}
+                  CRUD_Function={props.maintananceTicketDelete}
+                  id={arg._id}
+                  buttonName={"Delete"}
+                  message={"are you sure want to delete"}
+                />
               </div>
             );
           })
